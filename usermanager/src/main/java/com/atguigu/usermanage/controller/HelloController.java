e21c9bc9a25150be9abc7d7cda3186534779553c
dahai88 <dahai88@aliyun.com>
2019-07-20 11:14:25 +0800

    第一次使用小乌龟上传项目

---
+++ usermanager/src/main/java/com/atguigu/usermanage/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.atguigu.usermanage.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+    public void test(){
+        System.out.println("我是小乌龟，我也添加了一个方法");
+    }
+}
